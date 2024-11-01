ed87037970aa5699099e7d82823c6542aadb64e6
daquinteroflex <dario@flexcompute.com>
2024-11-01 17:10:46 +0100

    Ok getting the right problems now

--- autoflex/types/input.py
+++ autoflex/types/input.py
@@ -1,10 +1,11 @@
+from typing import Union
 import pydantic as pd
 import pydantic.v1 as pd1
 
-AutoflexInputClassTypes = \
-    pd._internal._model_construction.ModelMetaclass |
-    pd1._internal._model_construction.ModelMetaclass |
-    pd.BaseModel |
+AutoflexInputClassTypes = Union[
+    pd._internal._model_construction.ModelMetaclass,
+    pd.BaseModel,
     pd1.BaseModel
+]
 
 
